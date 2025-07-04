421e27ac77befe4d282c21b24a9d57f2d47e7600
user-season <hoaxuanhoan@gmail.com>
2025-07-04 08:25:35 +0700

    add update and delete functionality for user

--- server/routes/User.route.ts
+++ server/routes/User.route.ts
@@ -3,7 +3,8 @@ import { Router } from 'express';
 import { AuthController } from '../controllers/Auth.controller';
 import { authMiddleware, authorizeRole } from '../middlewares/Auth.middleware';
 import { validate } from '../middlewares/Validation.middleware'; // Import middleware validation
-import { loginSchema, registerSchema } from '../validation/AuthSchema.validation'; // Import schemas Zod
+import { loginSchema, registerSchema, updateSchema } from '../validation/AuthSchema.validation'; // Import schemas Zod
+
 
 const router = Router();
 const authController = new AuthController();
@@ -13,4 +14,12 @@ router.post('/register', validate(registerSchema), authController.register);
 
 // Route Đăng nhập (Login)
 router.post('/login', validate(loginSchema), authController.login);
+
+// Route Chỉnh sửa thông tin (Update)
+router.put('/update/:id', authMiddleware, validate(updateSchema), authController.update);
+
+// Route Xóa người dùng (Delete)
+router.delete('/delete/:id', authMiddleware, authController.delete);
+
+
 export default router;
