de909e754279b21ca0934311fc58ec343d003d49
ducc0506 <ducnd.ntd.vn@gmail.com>
2025-07-07 17:17:55 +0700

    lack

--- server/routes/Job.route.ts
+++ server/routes/Job.route.ts
@@ -1,24 +1,23 @@
 import { Router } from "express";
 import { JobController } from "../controllers/Job.controller";
-import { isRecruiter } from "../middlewares/role.middleware";
 
 
 const router = Router();
 const jobController = new JobController();
 
 // Route job recruitment
-router.post("/create", isRecruiter, jobController.createJob);
+router.post("/create", jobController.createJob);
 
 // Route get all jobs
 router.get("/", jobController.getAllJobs);
 
 // Route get job by id
-router.get("/:id", isRecruiter, jobController.getJobById);
+router.get("/:id", jobController.getJobById);
 
 // Route update job by id
-router.put("/:id", isRecruiter, jobController.updateJobById);
+router.put("/:id", jobController.updateJobById);
 
 // Route delete job by id
-router.delete("/:id", isRecruiter, jobController.deleteJobById);
+router.delete("/:id", jobController.deleteJobById);
 
 export default router;
